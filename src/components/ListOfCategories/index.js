d2556127d95f39c404db6616c0c1233be35d83c3
EdwinBaeza05 <edwinbaeza05@gmail.com>
2020-05-06 01:12:02 -0500

    first commit

---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import { Category } from '../Category'
+import { List, Item } from './style'
+import { Spinner } from './../Spinner'
+
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        setLoading(true)
+
+        setTimeout(
+          () => {
+            for (let index = 0; index < 10000; index++) {
+              console.log(index)
+            }
+          }, 100
+        )
+        const response = await window.fetch('https://petgram-server-edwin-dev.edwinbaeza05.now.sh/categories')
+        const categoriesResponse = await response.json()
+        setCategories(categoriesResponse)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.log('Error: ', error)
+      }
+    }
+    fetchCategories()
+  }, [])
+
+  return { categories, loading }
+}
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const onScroll = event => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        categories.map(
+          category =>
+            <Item key={category.id}>
+              <Category {... category} />
+            </Item>
+        )
+      }
+    </List>
+  )
+
+  if (loading) {
+    return <Spinner />
+  }
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  )
+}
---
+++ src/components/Spinner/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SpinnerLoading } from './styles'
+
+export const Spinner = (props) => {
+  console.log('Spinner')
+  return (
+    <SpinnerLoading>
+      <div></div>
+      <div></div>
+    </SpinnerLoading>
+  )
+}
